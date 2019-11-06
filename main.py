7c9c7119833b45ee962413f7f134423d25da3c15
Alexander Clements <alexfjclements@gmail.com>
2019-11-06 10:11:58 +0000

    added column headings to data

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import matplotlib as mpl
 
 data = pd.read_csv("log.csv")
 
-# TODO get column header list from data file
-headed_data = pd.DataFrame([data], columns=["Sequence", "Start", "End", "Coverage"])
+data.columns = ["Date/Time", "IP Address", "Latency", "Download", "Upload", "Provider"]
+
+print(data)

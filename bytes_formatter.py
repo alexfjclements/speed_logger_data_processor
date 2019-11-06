c28f302d8a0cc08b19e77065ad52ca307af2dc4e
Alexander Clements <alexfjclements@gmail.com>
2019-11-06 17:34:12 +0000

    got basic graph rendering, but needs unit conversion work amongst other things

---
+++ bytes_formatter.py
@@ -0,0 +1,46 @@
+"""
+Python format_bytes function, source:
+https://stackoverflow.com/questions/12523586/python-format-size-application-converting-b-to-kb-mb-gb-tb/37423778
+"""
+
+from datetime import datetime
+
+def format_bytes(size):
+    # 2**10 = 1024
+    power = 2**10
+    n = 0
+    power_labels = {0: '', 1: 'kilo', 2: 'mega', 3: 'giga', 4: 'tera'}
+    while size > power:
+        size /= power
+        n += 1
+    return size, power_labels[n]+'bytes'
+
+
+def format_speed(download, upload):
+    download_speeds = []
+    download_labels = []
+    upload_speeds = []
+    upload_labels = []
+
+    for entry in download:
+        speed, label = format_bytes(entry)
+        download_speeds.append(speed)
+        download_labels.append(label)
+
+    for entry in upload:
+        speed, label = format_bytes(entry)
+        upload_speeds.append(speed)
+        upload_labels.append(label)
+
+    # returning lists of labels, though not yet doing anything to process or compare them.
+    # TODO compare labels
+    return download_speeds, download_labels, upload_speeds, upload_labels
+
+
+def datetime_conversion(datetime_list):
+    converted_datetime = []
+
+    for timestamp in datetime_list:
+        converted_datetime.append(datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S"))
+
+    return converted_datetime
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pandas as pd
-import matplotlib as mpl
+import matplotlib.dates as mpl_d
+import matplotlib.pyplot as plt
+from datetime import datetime
+from bytes_formatter import format_speed, datetime_conversion
 
 data = pd.read_csv("log.csv")
 
-data.columns = ["Date/Time", "IP Address", "Latency", "Download", "Upload", "Provider"]
+data.columns = ["Date/Time", "IP Address", "Latency (m/s)", "Download Speed", "Upload Speed", "Provider"]
 
-print(data)
+download_speed_list = data["Download Speed"]
+upload_speed_list = data["Upload Speed"]
+timestamps = datetime_conversion(data["Date/Time"])
+
+download_speeds, download_labels, upload_speeds, upload_labels = format_speed(download_speed_list, upload_speed_list)
+
+# Basic label comparison
+if download_labels.count(download_labels[0]) == len(download_labels):
+    single_download_label = download_labels[0]
+
+if upload_labels.count(upload_labels[0]) == len(upload_labels):
+    single_upload_label = upload_labels[0]
+
+# Interesting, but requires solution to unit conversion issue to be properly meaningful
+mpl_timestamps = mpl_d.date2num(timestamps)
+plt.plot(timestamps, upload_speeds)
+plt.show()
